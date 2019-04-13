13f6839caf6a89172ae7a6bdd43ecadec76c2f15
Ansala <ansala1821@hotmail.com>
2019-04-13 17:38:46 -0400

    Changes

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "May Yuna",
+		photo: "https://dq1eylutsoz4u.cloudfront.net/2016/08/24175451/how-to-get-a-good-profile-photo.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Jean Belferaur",
+		photo: "https://image.shutterstock.com/image-photo/bearded-man-260nw-170081504.jpg",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Krish Tufford",
+		photo: "https://uk.match.com/pages/advice/wp-content/uploads/sites/32/2016/08/Profile-Tips-for-Men.jpg",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
